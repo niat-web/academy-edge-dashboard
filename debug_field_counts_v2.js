d9273ce3b737fbc0a9fb91ed66bbc3f7322ee166
GandotraDiya <diyagandotra02@gmail.com>
2026-01-10 11:06:58 +0530

    updating github account

--- debug_field_counts_v2.js
+++ debug_field_counts_v2.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 async function run() {
     const uri = "mongodb+srv://psm_db_user:PSMTeam-2024@academy-dashboard-clust.tpk215n.mongodb.net/academy-edge-dashboard?retryWrites=true&w=majority";
     const client = new MongoClient(uri);
-    
 
     try {
         await client.connect();
